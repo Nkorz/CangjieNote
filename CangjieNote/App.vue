2d78fd60bdddd2b9e2ad072aca479d2c69c6126b
srameo <870830732@qq.com>
2021-05-15 16:10:41 +0800

    配置云开发环境

--- CangjieNote/App.vue
+++ CangjieNote/App.vue
@@ -4,6 +4,17 @@
 		globalData: {
 			username: '白居易'
 		},
+    onLoad() {
+      wx.cloud.init({
+        // env 参数说明：
+        //   env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
+        //   此处请填入环境 ID, 环境 ID 可打开云控制台查看
+        //   如不填则使用默认环境（第一个创建的环境）    
+        //env: '你的环境ID',
+        env: 'cloud0-backend-7gnbnkiz459f6b99',
+        traceUser: true,
+      })
+    },
 		onLaunch() {
 			// 1.1.0版本之前关于http拦截器代码，已平滑移动到/common/http.interceptor.js中
 			// 注意，需要在/main.js中实例化Vue之后引入如下(详见文档说明)：
--- CangjieNote/vue.config.js
+++ CangjieNote/vue.config.js
@@ -1,10 +1,21 @@
+const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
 module.exports = {
 	// 配置路径别名
 	configureWebpack: {
 		devServer: {
 			// 调试时允许内网穿透，让外网的人访问到本地调试的H5页面
 			disableHostCheck: true
-		}
+		},
+    plugins: [
+        new CopyWebpackPlugin([
+            {
+                from: path.join(__dirname, 'cloudfunctions'),
+                to: path.join(__dirname, 'unpackage/dist', process.env.NODE_ENV === 'development' ? 'dev' : 'build', process.env.UNI_PLATFORM, 'cloudfunctions')
+            }
+        ])
+    ]
 	},
 	//productionSourceMap: false,
 }
