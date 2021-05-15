787c9de27bb2432f8577fb93bcc89ef4ee176f26
kirakira666 <847868144@qq.com>
2021-05-15 14:15:21 +0800

    Add UI components.

---
+++ CangjieNote/pages/welcome/welcome/welcome.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		heloworld
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
