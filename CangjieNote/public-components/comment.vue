4cf949bc928f218678e8f96a1a79c0d5cca5b2a3
hkx-hkx <1121143810@qq.com>
2021-05-16 12:54:14 +0800

    调整诗词详情页面，但还不可以正确传诗

--- CangjieNote/public-components/comment.vue
+++ CangjieNote/public-components/comment.vue
@@ -1,7 +1,8 @@
 <template>
 	<view>
 		<view class="comment" v-for="(res, index) in commentList" :key="res.id">
-			<view class="comment-body">
+			<view class="left"><image :src="res.url" mode="aspectFill"></image></view>
+			<view class="right">
 				<view class="top">
 					<view class="name">{{res.name}}</view>
 					<view class="like" :class="{highlight:res.isLike}">
@@ -84,12 +85,10 @@
 					}]
 			};
 		},
-		onLoad(){
-			this.getComment();
-		},
 		methods:{
 			//获取评论列表
 			getComment(){
+
 			},
 			// 点赞
 			getLike(index) {
@@ -108,43 +107,76 @@
 .comment {
 	display: flex;
 	padding: 30rpx;
-
-	.top {
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		margin-bottom: 10rpx;
-		.name {
-			color: #5677fc;
+	.left {
+		image {
+			width: 64rpx;
+			height: 64rpx;
+			border-radius: 50%;
+			background-color: #f2f2f2;
 		}
-		.like {
+	}
+	.right {
+		flex: 1;
+		padding-left: 20rpx;
+		font-size: 30rpx;
+		.top {
 			display: flex;
+			justify-content: space-between;
 			align-items: center;
-			color: #9a9a9a;
-			font-size: 26rpx;
-			.num {
-				margin-right: 4rpx;
-				color: #9a9a9a;
-			}
-		}
-		.highlight {
-			color: #5677fc;
-			.num {
+			margin-bottom: 10rpx;
+			.name {
 				color: #5677fc;
 			}
+			.like {
+				display: flex;
+				align-items: center;
+				color: #9a9a9a;
+				font-size: 26rpx;
+				.num {
+					margin-right: 4rpx;
+					color: #9a9a9a;
+				}
+			}
+			.highlight {
+				color: #5677fc;
+				.num {
+					color: #5677fc;
+				}
+			}
 		}
-	}
-	.content {
-		margin-bottom: 10rpx;
-	}
-	.bottom {
-		margin-top: 20rpx;
-		display: flex;
-		font-size: 24rpx;
-		color: #9a9a9a;
-		.reply {
-			color: #5677fc;
-			margin-left: 10rpx;
+		.content {
+			margin-bottom: 10rpx;
+		}
+		.reply-box {
+			background-color: rgb(242, 242, 242);
+			border-radius: 12rpx;
+			.item {
+				padding: 20rpx;
+				border-bottom: solid 2rpx $u-border-color;
+				.username {
+					font-size: 24rpx;
+					color: #999999;
+				}
+			}
+			.all-reply {
+				padding: 20rpx;
+				display: flex;
+				color: #5677fc;
+				align-items: center;
+				.more {
+					margin-left: 6rpx;
+				}
+			}
+		}
+		.bottom {
+			margin-top: 20rpx;
+			display: flex;
+			font-size: 24rpx;
+			color: #9a9a9a;
+			.reply {
+				color: #5677fc;
+				margin-left: 10rpx;
+			}
 		}
 	}
 }
