5e5cef5547e0252f498b1cd612572637959d06d2
srameo <870830732@qq.com>
2021-05-19 22:07:33 +0800

    新增getPoetryComments接口 - 用于获取诗词注释

---
+++ CangjieNote/cloudfunctions/getPoetryComments/index.js
@@ -0,0 +1,38 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init({
+  // env 参数说明：
+  //   env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
+  //   此处请填入环境 ID, 环境 ID 可打开云控制台查看
+  //   如不填则使用默认环境（第一个创建的环境）    
+  //env: '你的环境ID',
+  env: 'cloud0-backend-7gnbnkiz459f6b99',
+  traceUser: true,
+})
+
+const db = cloud.database();
+const _ = db.command;
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  var res = await db.collection("PoetryComments")
+                    .where({
+                      "poem_id": event.poemid
+                    })
+                    .get();
+  res = res.data;
+  if (res.length == 0) {
+    return {
+      code: -1,
+      err: "No Such Poem!",
+      data: null
+    };
+  }
+
+  return {
+    code: 0,
+    err: null,
+    data: res[0]
+  }
+}
