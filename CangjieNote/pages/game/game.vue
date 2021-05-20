aa425293b2dec91ee9545372a215cd4b48ec3d7c
kirakira666 <847868144@qq.com>
2021-05-20 11:43:18 +0800

    Show Char Split.

--- CangjieNote/pages/game/game.vue
+++ CangjieNote/pages/game/game.vue
@@ -1,43 +1,250 @@
 <template>
   <view>
+    <game :poemStr="poemStr" :charSplit="charSplit" ref="canvasRef" id="canvas-drag" :graph="graph" width="700" height="750" enableUndo="true"></game>
     {{poemStr}}
+    <view class="btn" @tap="onAddImage">添加图片</view>
+    <view class="btn" @tap="onAddTest">添加测试图片</view>
+    <view class="btn" @tap="onAddText('kkk',30,30)">添加文字</view>
+    <view class="btn" @tap="onExport">导出图片</view>
+    <view class="btn" @tap="onChangeColor">改变文字颜色</view>
+    <view class="btn" @tap="onChangeBgColor">改变背景颜色</view>
+    <view class="btn" @tap="onChangeBgImage">改变背景照片</view>
+    <view class="btn" @tap="onExportJSON">导出模板</view>
+    <view class="btn" @tap="onImport">导入默认模板</view>
+    <view class="btn" @tap="onClearCanvas">清空画布</view>
+    <view class="btn" @tap="onUndo">后退</view>
   </view>
 </template>
 
 <script>
+  import game from "../../public-components/game.vue";
   export default {
     data() {
       return {
-        poemStr:''
-
+        poemStr: '',
+        graph: {
+          list:[]
+        },
+        canvasBg: "",
+        shuffleStr: '',
+        charSplit: {
+          str: '',
+          list: []
+        }
       }
     },
+    onLoad(options) {
+      let poemStr = options.str;
+      let that = this;
+      that.poemStr = poemStr;
+      poemStr = this.randomCharSetter(poemStr);
+      let shuffleStr = this.shuffle(poemStr.split('')).join('').slice(0, 4)
+      this.shuffleStr = shuffleStr;
+      this.charSplit.str = shuffleStr;
+      console.log(shuffleStr)
+      console.log(poemStr)
+      wx.cloud.callFunction({
+        name: "gameInit",
+        data: {
+          sentence: shuffleStr,
+        },
+        success: function(res) {
+          that.charSplit.list = res.result.data;
+          console.log(that.charSplit.list)
+          for (let i=0;i<that.charSplit.list.length;++i){
+            let x = 275*Math.random()
+            let y = 280*Math.random()
+            console.log(that.charSplit.list[i].char)
+            that.onAddText(that.charSplit.list[i].char,x,y)
+          }
+          // that.poemId = res.result.data;
+        },
+        fail: function(error) {
+          console.log("error:", error);
+        },
+      });
+    },
+    components: {
+      game
+    },
     methods: {
-      onLoad(options) {
-        let poemStr = options.str;
-        let that = this;
-        that.poemStr=poemStr;
-        poemStr='关关雎鸠'
-        console.log(poemStr)
-        wx.cloud.callFunction({
-          name: "gameInit",
-          data: {
-            sentence: poemStr,
-          },
-          success: function(res) {
-            console.log(res)
-            // that.poemId = res.result.data;
-          },
-          fail: function(error) {
-            console.log("error:", error);
-          },
+      randomCharSetter(str) {
+        str = str.replaceAll('。', '')
+        str = str.replaceAll('，', '')
+        str = str.replaceAll(',', '')
+        str = str.replaceAll('！', '')
+        str = str.replaceAll('？', '')
+        // str=str.replace(/[\ |\~|\`|\!|\@|\#|\$|\%|\^|\&|\*|\(|\)|\-|\_|\+|\=|\||\\|\[|\]|\{|\}|\;|\:|\"|\'|\,|\<|\.|\>|\/|\?]/g,"");
+        // console.log(str);
+        return str;
+      },
+      shuffle(arr) {
+        let temp, length = arr.length;
+        for (let i = 0; i < length - 1; i++) {
+          let index = Math.floor(Math.random() * (length--));
+          temp = arr[index];
+          arr[index] = arr[length];
+          arr[length] = temp;
+        }
+        return arr;
+      },
+      /**
+       * 添加测试图片
+       */
+      onAddTest() {
+        this.graph.list.push({
+          w: 120,
+          h: 120,
+          type: 'image',
+          url: "/static/assets/images/test.png"
+        })
+        // this.setData({
+        //   graph: {
+        //     w: 120,
+        //     h: 120,
+        //     type: 'image',
+        //     url: "/static/assets/images/test.jpg"
+        //   }
+        // });
+      },
+
+      /**
+       * 添加图片
+       */
+      onAddImage() {
+        const that = this;
+        wx.chooseImage({
+          success: res => {
+            that.graph = {
+              w: 200,
+              h: 200,
+              type: 'image',
+              url: res.tempFilePaths[0]
+            }
+            // this.setData({
+            //   graph: {
+            //     w: 200,
+            //     h: 200,
+            //     type: 'image',
+            //     url: res.tempFilePaths[0]
+            //   }
+            // });
+          }
         });
       },
 
+      /**
+       * 添加文本
+       */
+      onAddText(char,ranX,ranY) {
+        this.graph.list.push({
+          type: 'text',
+          text: char,
+          x:ranX,
+          y:ranY
+        });
+        // this.setData({
+        //   graph: {
+        //     type: 'text',
+        //     text: 'helloworld'
+        //   }
+        // });
+      },
+
+      /**
+       * 导出图片
+       */
+      onExport() {
+        this.$refs.canvasRef.export().then(filePath => {
+          console.log(filePath);
+          wx.previewImage({
+            urls: [filePath]
+          });
+        }).catch(e => {
+          console.error(e);
+        });
+      },
+
+      /**
+       * 改变文字颜色
+       */
+      onChangeColor() {
+        this.$refs.canvasRef.changColor('blue');
+      },
+
+      /**
+       * 改变背景颜色
+       */
+      onChangeBgColor() {
+        this.$refs.canvasRef.changeBgColor('yellow');
+      },
+
+      /**
+       * 改变背景照片
+       */
+      onChangeBgImage() {
+        this.$refs.canvasRef.changeBgImage("/static/assets/images/test.jpg")
+      },
+
+      /**
+       * 导出当前画布为模板
+       */
+      onExportJSON() {
+        this.$refs.canvasRef.exportFun().then(imgArr => {
+          console.log(JSON.stringify(imgArr));
+        }).catch(e => {
+          console.error(e);
+        });
+      },
+
+      onImport() {
+        // 有背景
+        // let temp_theme = [{"type":"bgColor","color":"yellow"},{"type":"image","url":"../../assets/images/test.jpg","y":98.78423143832424,"x":143.78423143832424,"w":104.43153712335152,"h":104.43153712335152,"rotate":-12.58027482265038,"sourceId":null},{"type":"text","text":"helloworld","color":"blue","fontSize":24.875030530031438,"y":242.56248473498428,"x":119.57012176513672,"w":116.73966979980469,"h":34.87503053003144,"rotate":8.873370699754087}];
+        // 无背景
+        let temp_theme = [{
+          "type": "image",
+          "url": "/static/assets/images/test.jpg",
+          "y": 103,
+          "x": 91,
+          "w": 120,
+          "h": 120,
+          "rotate": 0,
+          "sourceId": null
+        }, {
+          "type": "text",
+          "text": "helloworld",
+          "color": "blue",
+          "fontSize": 20,
+          "y": 243,
+          "x": 97,
+          "rotate": 0
+        }];
+        this.$refs.canvasRef.initByArr(temp_theme);
+      },
+
+      onClearCanvas: function(event) {
+        let _this = this;
+        _this.canvasBg = null;
+        // _this.setData({
+        //   canvasBg: null
+        // });
+
+        this.$refs.canvasRef.clearCanvas();
+      },
+      onUndo: function(event) {
+        this.$refs.canvasRef.undo();
+      }
     }
+
   }
 </script>
 
 <style>
-
+  .btn {
+    padding: 10rpx 20rpx;
+    float: left;
+    margin: 10rpx;
+    border: solid 1px #dfdfdf;
+    border-radius: 10rpx;
+  }
 </style>
