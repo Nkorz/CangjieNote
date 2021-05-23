52775c0e4a3efc17bd9485060cf69d4df6e4d1f3
hkx-hkx <1121143810@qq.com>
2021-05-23 22:13:11 +0800

    详情页面信息为空的时候显示空元素

--- CangjieNote/pages/detail/detail.vue
+++ CangjieNote/pages/detail/detail.vue
@@ -1,224 +1,221 @@
- <template>
-  <view class="">
-    <poem-card-game :poem="poem" :thumb="false"></poem-card-game>
-    <u-line></u-line>
-    <u-subsection
-      :list="[{ name: '赏析' }, { name: '译文' }, { name: '注释' }]"
-      :current="current"
-      @change="sectionChange"
-    ></u-subsection>
-    <p class="analysis" v-if="current === 0">
-      {{ shangxi }}
-    </p>
-    <p class="analysis" v-else-if="current === 1">{{ fanyi }}</p>
-    <view class="analysis" v-else>
-      <view v-if="zhushi.length === 1">
-        <p>{{ zhushi }}</p>
-      </view>
-      <view v-else>
-        <ol v-for="(item, index) in zhushi" :key="item">
-          <li>{{ index + "." + item }}</li>
-        </ol>
-      </view>
-    </view>
-    <view class="menu" :class="{ active: menuFlag }">
-      <image
-        src="../../static/navi.svg"
-        class="menuTrigger"
-        @tap="clickMenu"
-      ></image>
-      <image
-        src="../../static/home.svg"
-        class="menuItem menuItem1"
-        @tap="toHome"
-      ></image>
-      <image
-        src="../../static/user.svg"
-        class="menuItem menuItem3"
-        @tap="toUser"
-      ></image>
-    </view>
-  </view>
+<template>
+	<view class="">
+		<poem-card-game :poem="poem" :thumb="false"></poem-card-game>
+		<u-line></u-line>
+		<u-subsection :list="[{ name: '赏析' }, { name: '译文' }, { name: '注释' }]" :current="current" :animation="true"
+			bgColor="#FFFFFF" active-color="#8C2E2E" @change="sectionChange"></u-subsection>
+		<view class="analysis" v-if="current === 0 && !isEmpty">
+			<p>{{ shangxi }}</p>
+		</view>
+		<u-empty class="empty-icon" v-else-if="current === 0 && isEmpty" text="暂无赏析"></u-empty>
+		<u-empty class="empty-icon" v-else-if="current === 1 && isEmpty" text="暂无翻译"></u-empty>
+		<view class="analysis" v-else-if="current === 1 && !isEmpty">
+			<p>{{ fanyi }}</p>
+		</view>
+		<u-empty class="empty-icon" v-else-if="current === 2 && isEmpty" text="暂无注释"></u-empty>
+		<view class="analysis" v-else>
+			<view v-if="zhushi.length === 1">
+				<p>{{ zhushi }}</p>
+			</view>
+			<view v-else>
+				<ol v-for="(item, index) in zhushi" :key="item">
+					<li>{{ index + "." + item }}</li>
+				</ol>
+			</view>
+		</view>
+		<view class="menu" :class="{ active: menuFlag }">
+			<image src="../../static/navi.svg" class="menuTrigger" @tap="clickMenu"></image>
+			<image src="../../static/home.svg" class="menuItem menuItem1" @tap="toHome"></image>
+			<image src="../../static/user.svg" class="menuItem menuItem3" @tap="toUser"></image>
+		</view>
+	</view>
 </template>
- 
- <script>
-import poemCardGame from "../../public-components/poem-card-game.vue";
-import comment from "../../public-components/comment.vue";
-export default {
-  data() {
-    return {
-      menuFlag: false,
-      poem: null,
-      shangxi: "加载中...",
-      fanyi: "加载中...",
-      zhushi: ["加载中..."],
-      current: 0,
-      borderTop: false,
-      midButton: true,
-    };
-  },
-  components: {
-    comment,
-    poemCardGame,
-  },
-  methods: {
-    sectionChange(current) {
-      this.current = current;
-    },
-    clickMenu() {
-      this.menuFlag = !this.menuFlag;
-    },
-    toUser() {
-      uni.reLaunch({
-        url: "/pages/user/user",
-      });
-    },
-    toHome() {
-      uni.reLaunch({
-        url: "/pages/main/main",
-      });
-    },
-  },
-  onLoad(options) {
-    let poemid = options.id;
-    let that = this;
-    // 通过poemId从后端获取诗
-    // 传递给[this.poem]
-    console.log("正在获取中，请等待");
-    // 通过poemId从后端获取诗和解析
-    // 传递给[this.poem]和[this.analysis]
-    wx.cloud.callFunction({
-      name: "getPoemUseId",
-      data: {
-        poemid: poemid,
-      },
-      success: function (res) {
-        that.poem = res.result.data;
-      },
-      fail: function (error) {
-        console.log("error:", error);
-      },
-    });
-    wx.cloud.callFunction({
-      name: "getPoetryComments",
-      data: {
-        poemid: poemid,
-      },
-      success: function (res) {
-        let analysis = res.result.data;
-        console.log(analysis);
-        if (analysis === null) {
-          that.shangxi = "暂无赏析";
-          that.fanyi = "暂无翻译";
-          that.zhushi = ["暂无注释"];
-          return;
-        }
-        let shangxi = analysis.shangxi[0];
-        shangxi = shangxi.split(/[\u3000\u25b2]/);
-        shangxi.pop();
-        shangxi.shift();
-        let tmpShangxi = [];
-        for (let i = 0; i < shangxi.length; i++) {
-          if (shangxi[i] !== "") {
-            shangxi[i] = shangxi[i].replace("\n", "");
-            shangxi[i] = "\u3000" + shangxi[i];
-            tmpShangxi.push(shangxi[i]);
-          }
-        }
-        that.shangxi = tmpShangxi;
-        that.zhushi = analysis.zhushi[0];
-        that.fanyi = "\u3000" + analysis.fanyi;
-        if (analysis.zhushi.length === 0) {
-          that.zhushi = ["暂无注释"];
-        }
-        if (analysis.fanyi.length === 0) {
-          that.fanyi = "暂无翻译";
-        }
-      },
-      fail: function (error) {
-        console.log("error:", error);
-      },
-    });
-  },
-};
+
+<script>
+	import poemCardGame from "../../public-components/poem-card-game.vue";
+	import comment from "../../public-components/comment.vue";
+	export default {
+		data() {
+			return {
+				menuFlag: false,
+				poem: null,
+				shangxi: "加载中...",
+				fanyi: "加载中...",
+				zhushi: ["加载中..."],
+				current: 0,
+				borderTop: false,
+				midButton: true,
+				isEmpty:false
+			};
+		},
+		components: {
+			comment,
+			poemCardGame,
+		},
+		methods: {
+			sectionChange(current) {
+				this.current = current;
+			},
+			clickMenu() {
+				this.menuFlag = !this.menuFlag;
+			},
+			toUser() {
+				uni.reLaunch({
+					url: "/pages/user/user",
+				});
+			},
+			toHome() {
+				uni.reLaunch({
+					url: "/pages/main/main",
+				});
+			},
+		},
+		onLoad(options) {
+			let poemid = options.id;
+			let that = this;
+			// 通过poemId从后端获取诗
+			// 传递给[this.poem]
+			console.log("正在获取中，请等待");
+			// 通过poemId从后端获取诗和解析
+			// 传递给[this.poem]和[this.analysis]
+			wx.cloud.callFunction({
+				name: "getPoemUseId",
+				data: {
+					poemid: poemid,
+				},
+				success: function(res) {
+					that.poem = res.result.data;
+				},
+				fail: function(error) {
+					console.log("error:", error);
+				},
+			});
+			wx.cloud.callFunction({
+				name: "getPoetryComments",
+				data: {
+					poemid: poemid,
+				},
+				success: function(res) {
+					let analysis = res.result.data;
+					console.log(analysis);
+					if (analysis === null) {
+						that.isEmpty = true;
+						that.shangxi = "暂无赏析";
+						that.fanyi = "暂无翻译";
+						that.zhushi = ["暂无注释"];
+						return;
+					}
+					let shangxi = analysis.shangxi[0];
+					shangxi = shangxi.split(/[\u3000\u25b2]/);
+					shangxi.pop();
+					shangxi.shift();
+					let tmpShangxi = [];
+					for (let i = 0; i < shangxi.length; i++) {
+						if (shangxi[i] !== "") {
+							shangxi[i] = shangxi[i].replace("\n", "");
+							shangxi[i] = "\u3000" + shangxi[i];
+							tmpShangxi.push(shangxi[i]);
+						}
+					}
+					that.shangxi = tmpShangxi;
+					that.zhushi = analysis.zhushi[0];
+					that.fanyi = "\u3000" + analysis.fanyi;
+					if (analysis.zhushi.length === 0) {
+						that.zhushi = ["暂无注释"];
+					}
+					if (analysis.fanyi.length === 0) {
+						that.fanyi = "暂无翻译";
+					}
+				},
+				fail: function(error) {
+					console.log("error:", error);
+				},
+			});
+		},
+	};
 </script>
- 
+
 <style scoped lang="scss">
-.menu {
-  position: fixed;
-  width: 110rpx;
-  height: 110rpx;
-  bottom: 120rpx;
-  right: 44rpx;
-  border-radius: 50%;
-}
+	.menu {
+		position: fixed;
+		width: 110rpx;
+		height: 110rpx;
+		bottom: 120rpx;
+		right: 44rpx;
+		border-radius: 50%;
+	}
 
-.menuTrigger {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 80rpx;
-  height: 80rpx;
-  background-color: rgba(102, 175, 123, 0.2);
-  border-radius: 50%;
-  padding: 20rpx;
-  cursor: pointer;
-  transition: 0.35s ease;
-}
+	.menuTrigger {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 80rpx;
+		height: 80rpx;
+		background-color: rgba(102, 175, 123, 0.2);
+		border-radius: 50%;
+		padding: 20rpx;
+		cursor: pointer;
+		transition: 0.35s ease;
+	}
 
-.menuItem {
-  position: absolute;
-  width: 60rpx;
-  height: 60rpx;
-  top: 10rpx;
-  left: 10rpx;
-  padding: 20rpx;
-  border-radius: 50%;
-  background-color: rgba(133, 162, 175, 0.2);
-  border: none;
-  box-shadow: 0 0 5rpx 1rpx rgba(0, 0, 0, 0.05);
-  z-index: -1000;
-  opacity: 0;
-}
+	.menuItem {
+		position: absolute;
+		width: 60rpx;
+		height: 60rpx;
+		top: 10rpx;
+		left: 10rpx;
+		padding: 20rpx;
+		border-radius: 50%;
+		background-color: rgba(133, 162, 175, 0.2);
+		border: none;
+		box-shadow: 0 0 5rpx 1rpx rgba(0, 0, 0, 0.05);
+		z-index: -1000;
+		opacity: 0;
+	}
 
-.menuItem1 {
-  transition: 0.35s ease;
-}
+	.menuItem1 {
+		transition: 0.35s ease;
+	}
 
-.menuItem2 {
-  transition: 0.35s ease 0.1s;
-}
+	.menuItem2 {
+		transition: 0.35s ease 0.1s;
+	}
 
-.menuItem3 {
-  transition: 0.35s ease 0.2s;
-}
+	.menuItem3 {
+		transition: 0.35s ease 0.2s;
+	}
 
-.menu.active .menuTrigger {
-  transform: rotateZ(225deg);
-  background-color: rgba(102, 175, 123, 0.3);
-}
+	.menu.active .menuTrigger {
+		transform: rotateZ(225deg);
+		background-color: rgba(102, 175, 123, 0.3);
+	}
 
-.menu.active .menuItem1 {
-  top: -106rpx;
-  left: -120rpx;
-  opacity: 1;
-}
+	.menu.active .menuItem1 {
+		top: -106rpx;
+		left: -120rpx;
+		opacity: 1;
+	}
 
-.menu.active .menuItem2 {
-  top: 10rpx;
-  left: -164rpx;
-  opacity: 1;
-}
+	.menu.active .menuItem2 {
+		top: 10rpx;
+		left: -164rpx;
+		opacity: 1;
+	}
 
-.menu.active .menuItem3 {
-  top: 126rpx;
-  left: -120rpx;
-  opacity: 1;
-}
+	.menu.active .menuItem3 {
+		top: 126rpx;
+		left: -120rpx;
+		opacity: 1;
+	}
 
-.analysis {
-  margin: 20rpx;
-  white-space: pre-wrap;
-}
+	.analysis {
+		margin: 60rpx;
+		white-space: pre-wrap;
+	}
+	
+	.empty-icon {
+		display: flex;
+		margin-top: 100rpx;
+		justify-content: center;
+	}
 </style>
- 
--- CangjieNote/pages/user/user.vue
+++ CangjieNote/pages/user/user.vue
@@ -5,171 +5,170 @@
 		<!-- 数据列表 -->
 		<view v-if="isShowCard">
 			<view v-for="(item,index) in userCollections" :key="index">
-			  <user-poem-card :poem="item" :thumb="true" @routerChange="toDetail"></user-poem-card>
+				<user-poem-card :poem="item" :thumb="true" @routerChange="toDetail"></user-poem-card>
 			</view>
 		</view>
-    <view class="menu" :class="{active:menuFlag}">
-      <image src="../../static/navi.svg" class="menuTrigger" @tap="clickMenu"></image>
-      <image src="../../static/home.svg" class="menuItem menuItem1" @tap="toHome"></image>
-      <image src="../../static/user.svg" class="menuItem menuItem3" @tap="toUser"></image>
-    </view>
-  </view>
+		<view class="menu" :class="{active:menuFlag}">
+			<image src="../../static/navi.svg" class="menuTrigger" @tap="clickMenu"></image>
+			<image src="../../static/home.svg" class="menuItem menuItem1" @tap="toHome"></image>
+			<image src="../../static/user.svg" class="menuItem menuItem3" @tap="toUser"></image>
+		</view>
+	</view>
 </template>
 
 <script>
 	import userInfo from "../../public-components/user-info.vue"
 	import userPoemCard from "../../public-components/user-poem-card"
-export default{
-	data(){
-		return {
-      mask: false,
-      menuFlag: false,
-			isShowCard:false,
-			userCollections:[]
-		}
-	},
-	components:{
-		userInfo,
-		userPoemCard
-	},
-	watch: {
-		isShowCard:{
-			immediate:true,
-			handler:function(val){
-				console.log("change!",val);
+	export default {
+		data() {
+			return {
+				mask: false,
+				menuFlag: false,
+				isShowCard: false,
+				userCollections: []
 			}
-		}
-	},
-	onShow() {
-		let that = this;
-		let tmp = {};
-		// 获取当前用户收藏的诗词信息
-		wx.cloud.callFunction({
-			name:'getCollection',
-			success:res=>{
-				console.log(res);
-				let collections = res.result.data;
-				that.userCollections = [];
-				// this.userCollections = res.result.data;
-				collections.forEach(function(item,index){
-					tmp = {
-						id: item._id,
-						title: item.title,
-						author: item.flag,
-						star: true,
-						starNum: item.stars,
-						content: item.content
+		},
+		components: {
+			userInfo,
+			userPoemCard
+		},
+		watch: {
+			isShowCard: {
+				immediate: true,
+				handler: function(val) {
+					console.log("change!", val);
+					if (val) {
+						this.getUserCollections();
 					}
-					that.userCollections.push(tmp);
-				})
-				console.log(this.userCollections);
+				}
 			}
-		})
-	},
-  components: {
-      userInfo,
-      userPoemCard
-  },
-	methods:{
-    toUser(){
-      uni.reLaunch({
-        url: "/pages/user/user",
-      })
-    },
-    toHome(){
-    	uni.reLaunch({
-    	  url: "/pages/main/main",
-    	});
-    },
-		clickMenu(){
-			this.menuFlag = !this.menuFlag;
 		},
-		showCard(data){
-			this.isShowCard = data;
-		},
-		toDetail(id) {
-		  this.$u.route({
-		    url: "/pages/detail/detail",
-		    animationType: "slide-in-bottom",
-		    params: {
-		      id: id
-		    },
-		  });
-		},
+		methods: {
+			getUserCollections() {
+				let that = this;
+				let tmp = {};
+				// 获取当前用户收藏的诗词信息
+				wx.cloud.callFunction({
+					name: 'getCollection',
+					success: res => {
+						console.log(res);
+						let collections = res.result.data;
+						that.userCollections = [];
+						// this.userCollections = res.result.data;
+						collections.forEach(function(item, index) {
+							tmp = {
+								id: item._id,
+								title: item.title,
+								author: item.flag,
+								star: true,
+								starNum: item.stars,
+								content: item.content
+							}
+							that.userCollections.push(tmp);
+						})
+						console.log(this.userCollections);
+					}
+				})
+			},
+			toUser() {
+				uni.reLaunch({
+					url: "/pages/user/user",
+				})
+			},
+			toHome() {
+				uni.reLaunch({
+					url: "/pages/main/main",
+				});
+			},
+			clickMenu() {
+				this.menuFlag = !this.menuFlag;
+			},
+			showCard(data) {
+				this.isShowCard = data;
+			},
+			toDetail(id) {
+				this.$u.route({
+					url: "/pages/detail/detail",
+					animationType: "slide-in-bottom",
+					params: {
+						id: id
+					},
+				});
+			},
+		}
 	}
-}
 </script>
 
 <style>
-  .menu {
-    position: fixed;
-    width: 110rpx;
-    height: 110rpx;
-    bottom: 120rpx;
-    right: 44rpx;
-    border-radius: 50%;
-  }
+	.menu {
+		position: fixed;
+		width: 110rpx;
+		height: 110rpx;
+		bottom: 120rpx;
+		right: 44rpx;
+		border-radius: 50%;
+	}
 
-  .menuTrigger {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 80rpx;
-    height: 80rpx;
-    background-color: rgba(102, 175, 123, 0.2);
-    border-radius: 50%;
-    padding: 20rpx;
-    cursor: pointer;
-    transition: .35s ease;
-  }
+	.menuTrigger {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 80rpx;
+		height: 80rpx;
+		background-color: rgba(102, 175, 123, 0.2);
+		border-radius: 50%;
+		padding: 20rpx;
+		cursor: pointer;
+		transition: .35s ease;
+	}
 
-  .menuItem {
-    position: absolute;
-    width: 60rpx;
-    height: 60rpx;
-    top: 10rpx;
-    left: 10rpx;
-    padding: 20rpx;
-    border-radius: 50%;
-    background-color: rgba(133, 162, 175, 0.2);
-    border: none;
-    box-shadow: 0 0 5rpx 1rpx rgba(0, 0, 0, .05);
-    z-index: -1000;
-    opacity: 0;
-  }
+	.menuItem {
+		position: absolute;
+		width: 60rpx;
+		height: 60rpx;
+		top: 10rpx;
+		left: 10rpx;
+		padding: 20rpx;
+		border-radius: 50%;
+		background-color: rgba(133, 162, 175, 0.2);
+		border: none;
+		box-shadow: 0 0 5rpx 1rpx rgba(0, 0, 0, .05);
+		z-index: -1000;
+		opacity: 0;
+	}
 
-  .menuItem1 {
-    transition: .35s ease;
-  }
+	.menuItem1 {
+		transition: .35s ease;
+	}
 
-  .menuItem2 {
-    transition: .35s ease .1s;
-  }
+	.menuItem2 {
+		transition: .35s ease .1s;
+	}
 
-  .menuItem3 {
-    transition: .35s ease .2s;
-  }
+	.menuItem3 {
+		transition: .35s ease .2s;
+	}
 
-  .menu.active .menuTrigger {
-    transform: rotateZ(225deg);
-    background-color: rgba(102, 175, 123, 0.3);
-  }
+	.menu.active .menuTrigger {
+		transform: rotateZ(225deg);
+		background-color: rgba(102, 175, 123, 0.3);
+	}
 
-  .menu.active .menuItem1 {
-    top: -106rpx;
-    left: -120rpx;
-    opacity: 1;
-  }
+	.menu.active .menuItem1 {
+		top: -106rpx;
+		left: -120rpx;
+		opacity: 1;
+	}
 
-  .menu.active .menuItem2 {
-    top: 10rpx;
-    left: -164rpx;
-    opacity: 1;
-  }
+	.menu.active .menuItem2 {
+		top: 10rpx;
+		left: -164rpx;
+		opacity: 1;
+	}
 
-  .menu.active .menuItem3 {
-    top: 126rpx;
-    left: -120rpx;
-    opacity: 1;
-  }
+	.menu.active .menuItem3 {
+		top: 126rpx;
+		left: -120rpx;
+		opacity: 1;
+	}
 </style>
